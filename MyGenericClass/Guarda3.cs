85d8799cc9b9441608b568b9e6e8063920ea9985
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-22 12:59:52 +0100

    Criado um construtor vazio

--- MyGenericClass/Guarda3.cs
+++ MyGenericClass/Guarda3.cs
@@ -7,5 +7,12 @@ namespace MyGenericClass
         private T itemZero;
         private T itemOne;
         private T itemTwo;
+
+        public Guarda3()
+        {
+            itemZero = default(T);
+            itemOne = default(T);
+            itemTwo = default(T);
+        }
     }
 }
