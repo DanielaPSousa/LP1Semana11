79ebf6cf0e62cb4988471e42c067ba290f4cd6c0
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-22 12:31:31 +0100

    Método GetTheOnesBefore que aceita um enumerável de itens e usa uma restrição para garantir que T seja um tipo de valor

--- TheyCameBefore/BeforeUtils.cs
+++ TheyCameBefore/BeforeUtils.cs
@@ -4,8 +4,11 @@ using System.Linq;
 
 namespace TheyCameBefore
 {
-    public class BeforeUtils
+    public static class BeforeUtils
     {
-        
+        public static IEnumerable<T> GetTheOnesBefore<T>(IEnumerable<T> items, T item) where T : struct, IComparable<T>
+        {
+            return items.Where(i => i.CompareTo(item) < 0);
+        }
     }
 }
