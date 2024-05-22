65124a5a63fee830e37e14f8b66f3c6cf46d72b8
Daniela Sousa <danielasousa.info@gmail.com>
2024-05-22 12:19:18 +0100

    Criado Método Static que devolve o número de itens num enumerável

--- HowManyOfThisType/Checker.cs
+++ HowManyOfThisType/Checker.cs
@@ -1,12 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace HowManyOfThisType
 {
     public static class Checker
     {
-        
+        public static int HowManyOfType<T>(IEnumerable<object> items)
+        {
+            return items.Count(item => item is T);
+        }
     }
 }
